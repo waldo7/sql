26b109a63fbc736d89460c7f8a4f93281d5f57d4
Waldo Ramones <waldo.ramones@gmail.com>
2016-09-13 21:10:52 +0800

    Executemany method

---
+++ sqlc.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+with sqlite3.connect("new.db") as conn:
+    cur = conn.cursor()
+    cities = [
+        ('Boston', 'MA', 600000),
+        ('Chicago', 'IL', 2700000),
+        ('Houston', 'TX', 2100000),
+        ('Phoenix', 'AZ', 1500000)
+    ]
+    cur.executemany("""INSERT INTO population VALUES (?, ?, ?)""", cities)
